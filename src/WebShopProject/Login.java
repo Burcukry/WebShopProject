ebb60a89d240a70a6d60d9588fc343435452f16a
yarensanli <yarensalmann@gmail.com>
2023-07-28 11:27:12 -0700

    yaren

--- src/WebShopProject/Login.java
+++ src/WebShopProject/Login.java
@@ -29,4 +29,25 @@ public class Login extends BaseDriver {
 
     }
 
+    @Test
+    public void test2() {
+        driver.get("https://demowebshop.tricentis.com/");
+
+        WebElement logIn = driver.findElement(By.xpath("//*[text()='Log in']"));
+        logIn.click();
+
+        WebElement userName = driver.findElement(By.id("Email"));
+        userName.sendKeys("stolegen257@gmail.com");
+
+        WebElement passWord = driver.findElement(By.id("Password"));
+        passWord.sendKeys("st12345");
+
+        WebElement loginBttn = driver.findElement(By.cssSelector("input[value='Log in']"));
+        loginBttn.click();
+
+        WebElement successLogin = driver.findElement(By.xpath("//span[.='Login was unsuccessful. Please correct the errors and try again.']"));
+
+        Assert.assertTrue(successLogin.getText().contains("unsuccessful"), "Test failed");
+    }
+
 }
